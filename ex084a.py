727a8fee1a96497fd3fb9a6036a563cd85d3b2bb
Emerson Felippini <felippini2015@outlook.com>
2022-01-19 21:39:25 -0300

    Update ex084a.py

--- ex084a.py
+++ ex084a.py
@@ -5,6 +5,13 @@ quantidade = maior_peso = menor_peso = 0
 while True:
     dados.append(str(input('Digite o nome: ')))
     dados.append(float(input('Digite o peso: ')))
+    if len(lista) == 0:
+        maior_peso = menor_peso = dados[1]
+    else:
+        if dados[1] > maior_peso:
+            maior_peso = dados[1]
+        if dados[1] < menor_peso:
+            menor_peso = dados[1]
     quantidade += 1
     lista.append(dados[:])
     dados.clear()
@@ -14,14 +21,6 @@ while True:
     while escolha not in 'SN':
         print('Escolha inválida.')
         escolha = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
-for pos, cont in enumerate(lista):
-    if pos == 0:
-        maior_peso = cont[pos][1]
-        menor_peso = cont[pos][1]
-    if cont[pos][1] > maior_peso:
-        maior_peso = cont[pos][1]
-    elif cont[pos][1] < menor_peso:
-        menor_peso = cont[pos][1]
 print(f'Foram cadastradas: {quantidade} pessoas.')
-print(f'Tiveram {maior_peso} com o maior peso.')
-print(f'Tiveram {menor_peso} com o menor peso.')
+print(f'O maior peso foi de {maior_peso}KG, e foram cadastradas {} pessoas com esse peso.')
+print(f'O menor peso foi de {menor_peso}KG, e foram cadastradas {} pessoas com esse peso.')
